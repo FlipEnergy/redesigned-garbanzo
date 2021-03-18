65e226ae20e7d28fe95d643ce0500e615dd8684e
Dennis Zhang <dennis.zhang.nrg@gmail.com>
2021-03-17 23:18:23 -0700

    CICD test 3

--- concourse_pipelines/deploy_to_k8s.sh
+++ concourse_pipelines/deploy_to_k8s.sh
@@ -1,20 +1,20 @@
 #!/bin/sh -e
 
-echo "$KEY_JSON" > service_account_key.json
-ls -lh service_account_key.json
+echo "$KEY_JSON" > /tmp/service_account_key.json
+ls -lh /tmp/service_account_key.json
 
 GARBANZO_TAG=`cat redesigned-garbanzo/.git/short_ref`
 export GARBANZO_TAG
 
 echo "deploying with ${GARBANZO_TAG}"
 
-gcloud auth activate-service-account --key-file=service_account_key.json
+gcloud auth activate-service-account --key-file=/tmp/service_account_key.json
 gcloud container clusters get-credentials "$CLUSTER" --zone "$ZONE" --project "$PROJECT"
 
-kubectl -n concourse get secret gpg-key -o jsonpath='{.data.secretKey}' | base64 -d > secretKey.asc
+kubectl -n concourse get secret gpg-key -o jsonpath='{.data.secretKey}' | base64 -d > /tmp/secretKey.asc
 
 echo "Importing secret key..."
-gpg --import secretKey.asc
+gpg --import /tmp/secretKey.asc
 
 cd redesigned-garbanzo
 echo
